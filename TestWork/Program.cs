21c68c9f9d9bf316c61c00a7f09f6e9b3ef5037f
Maksim <md.sinikov@gmail.com>
2022-09-15 11:33:41 +0500

    Написал функцию для определения длинны входного массива

--- TestWork/Program.cs
+++ TestWork/Program.cs
@@ -1,2 +1,24 @@
-﻿
+﻿// string n = "123a";
+// bool check = int.TryParse(n,out int num);
+// System.Console.WriteLine(check);
 
+int intMassivLength(string input)
+{
+    for (int i = 0; i < 1; i++)
+    {
+        if(int.TryParse(input, out int num))
+        {
+            return Convert.ToInt32(input);
+        }
+        else
+        System.Console.WriteLine("Вы ввели не число!");
+        System.Console.Write("Введите длину задаваемого массива:");
+        input = Console.ReadLine();
+        i--;
+    }
+    return 0;
+}
+
+
+System.Console.Write("Введите длину задаваемого массива: ");
+int massivLen = intMassivLength(Console.ReadLine());
